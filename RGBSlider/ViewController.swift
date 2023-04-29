dc7ddb79c0f0cf7740aad93d27ff2e7147129cf2
Alexey Zinovyev <zinovkin@gmail.com>
2023-04-29 17:19:52 +0300

    add logical

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueValue: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        sliderActions()
+        
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        sliderActions()
+    }
+    
+    @IBAction func greenSliderAction() {
+        sliderActions()
+    }
+    
+    @IBAction func blueSliderAction() {
+        sliderActions()
+    }
+    
+    private func sliderActions () {
+        redValue.text = String(format: "%.2f", redSlider.value)
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+        blueValue.text = String(format: "%.2f", blueSlider.value)
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
 }
 
